fb3888517c6d7a60fd5c5c6b08a5dfe634abd95d
Damian Kuriata <damianisko7.dk@gmail.com>
2023-05-06 13:22:42 +0100

    Clean Popup and Options tsx and css

--- src/views/Options/App.tsx
+++ src/views/Options/App.tsx
@@ -3,20 +3,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Options page</p>
-        <p>
-          Edit <code>src/views/Options/App.tsx</code> and save.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <p>Options</p>
       </header>
     </div>
   );
