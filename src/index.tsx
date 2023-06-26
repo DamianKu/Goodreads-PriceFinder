db78a7a98a670b21f67a888000c5fdd8cf627ea6
Damian Kuriata <damianisko7.dk@gmail.com>
2023-06-26 18:58:03 +0100

    Provide a store to Popup app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'webext-redux';
 import App from './views/Popup/App';
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-); 
+const store = new Store();
+Object.assign(store, {
+  dispatch: store.dispatch.bind(store),
+  getState: store.getState.bind(store),
+  subscribe: store.subscribe.bind(store),
+});
+
+// wait for the store to connect to the background page
+store.ready().then(() => {
+  ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+      document.getElementById('root'),
+  );
+});
+
