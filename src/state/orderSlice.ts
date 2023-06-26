ce764b6e07870ea7f275c1dfc35b4db35f1b4a92
Damian Kuriata <damianisko7.dk@gmail.com>
2023-06-26 18:59:10 +0100

    Create order state management

---
+++ src/state/orderSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export type Order = { id: string, visible: boolean }[];
+
+export interface State {
+  order: Order;
+}
+
+export const orderSlice = createSlice({
+  name: 'order',
+  initialState: {
+    order: [
+      {id: 'Paperback', visible: true},
+      {id: 'Hardcover', visible: true},
+      {id: 'Kindle Edition', visible: true},
+      {id: 'Audiobook', visible: true},
+      {id: 'Audio CD', visible: true},
+      {id: 'Spiral-bound', visible: true},
+    ],
+  } as State,
+  reducers: {
+    setNewOrder: (state, action: { type: string, payload: Order }) => {
+      state.order = action.payload;
+    },
+  },
+});
+
+export const {setNewOrder} = orderSlice.actions;
+
+export const selectOrder = (s: { order: State }) => s.order.order;
+
+export default orderSlice.reducer;
