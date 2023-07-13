7f8c1093724f86b2f7c59b5097571be5db87df05
Damian Kuriata <damianisko7.dk@gmail.com>
2023-07-13 18:09:46 +0100

    Persist book order using localStorage

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,14 +1,26 @@
-import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
 import orderReducer from './orderSlice';
 import booksReducer from './booksSlice';
+import { localStorage } from "redux-persist-webextension-storage";
+import { persistStore, persistReducer } from 'redux-persist'
+import { wrapStore } from "webext-redux";
 
 export const listenerMiddleware = createListenerMiddleware();
 
-export default configureStore({
-  reducer: {
-    order: orderReducer,
-    books: booksReducer,
-  },
+const orderStorageConfig = {
+  key: 'gpf-order',
+  storage: localStorage,
+}
+
+const rootReducer = combineReducers({
+  order: persistReducer(orderStorageConfig, orderReducer),
+  books: booksReducer
+})
+
+export const store = configureStore({
+  reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware()
       .prepend(listenerMiddleware.middleware)
 });
+wrapStore(store);
+persistStore(store)
